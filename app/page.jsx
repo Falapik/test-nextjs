0c3abcbc27e665375d0143639aba18b50c33b3b0
Kolya Kolya <kolya.dima.caca@mail.ru>
2024-09-26 04:02:26 +0300

    add product list and feedback window

---
+++ app/page.jsx
@@ -0,0 +1,198 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <>
+        <>
+          <>
+            <>
+              <>
+                <>
+                  <>
+                    <div className="flex items-center justify-center h-52">
+                      <h1 className="text-center text-8xl font-bold">
+                        GIVENCHY
+                      </h1>
+                    </div>
+                    <div className=" flex justify-center">
+                      <div className="flex gap-20 flex-col m-[97px]">
+                        <div className="text-4xl font-medium max-w-[638px] w-full mr-[90px]">
+                          COUTURE MAKEUP. Рассыпчатая пудра и скользящая помада
+                          с ультра-тонкой текстурой придают коже естественный
+                          цвет, при этом матируя и подсвечивая её.
+                        </div>
+                        <button className="bg-[#600027] w-[344px] h-[85px] rounded-[52px] text-3xl">
+                          Купить от 29 000 ₽
+                        </button>
+                      </div>
+                      <div className="translate-x-[-100px] translate-y-[-70px]">
+                        <Image
+                          src="/image.svg"
+                          width={600}
+                          height={834}
+                          alt="test"
+                        />
+                      </div>
+                    </div>
+                    <div className="text-3xl mx-[90px] my-6">
+                      <h1>
+                        Французский модный дом Givenchy представил кутюрную
+                        коллекцию макияжа, которая вышла лимитированным тиражом.
+                        В летнюю коллекцию Givenchy Couture Makeup Collection
+                        2017 вошли: - рассыпчатая пудра
+                      </h1>
+                    </div>
+                  </>
+                  <div className="max-w-full h-3 mx-[90px]">
+                    <div className="w-full h-0.5 bg-white "></div>
+                  </div>
+                </>
+                <div className="max-w-full h-[210px] mx-[90px] flex justify-end">
+                  <div className="h-[210px] w-[3px] bg-white translate-y-[-10px]"></div>
+                </div>
+              </>
+              <div className="flex mx-[90px] justify-between translate-y-[-150px]">
+                <div className="text-8xl">GIVENCHY — это</div>
+                <div className="text-[40px] mx-[90px]">
+                  <li>естественный цвет</li>
+                  <li>качественный материал</li>
+                  <li>удобный кейс</li>
+                  <li>натуральный продукт</li>
+                </div>
+              </div>
+            </>
+            <div className="h-[806px] w-full bg-white flex">
+              <div className="mt-5 mb-0 ml-24 px-7 py-7">
+                <div className="text-black font-sans font-medium text-[40px] tracking-[-0.01em] w-[428px]">
+                  COUTURE MAKEUP COLLECTION 2017
+                </div>
+                <div className="font-medium text-[20px] tracking-[0.01em] text-black w-[271px]">
+                  Для людей, которые знают, что такое росскошь
+                </div>
+                <div className="">
+                  <Image src="./first.svg" width={496} height={496}></Image>
+                </div>
+                <div className="flex items-center justify-between w-[500px]">
+                  <div className="font-bold text-4xl tracking-tight text-black">
+                    29 000 ₽
+                  </div>
+                  <button className="bg-[#600027] rounded-[52px] w-[157px] h-[63px]  text-[25px] translate-x-[-20px]">
+                    Купить
+                  </button>
+                </div>
+              </div>
+              <div className="h-full w-[3px] bg-black"></div>
+              <div className="mt-5 mb-0 ml-12 px-7 py-7">
+                <div className="text-black font-sans font-medium text-[40px] tracking-[-0.01em] w-[428px]">
+                  COUTURE MAKEUP COLLECTION 2017
+                </div>
+                <div className="font-medium text-[20px] tracking-[0.01em] text-black w-[271px]">
+                  Для людей, которые знают, что такое росскошь
+                </div>
+                <div className="">
+                  <Image src="./first.svg" width={496} height={496}></Image>
+                </div>
+                <div className="flex items-center justify-between w-[500px]">
+                  <div className="font-bold text-4xl tracking-tight text-black">
+                    29 000 ₽
+                  </div>
+                  <button className="bg-[#600027] rounded-[52px] w-[157px] h-[63px]  text-[25px] translate-x-[-20px]">
+                    Купить
+                  </button>
+                </div>
+              </div>
+              <div className="h-full w-[3px] bg-black"></div>
+              <div className="mt-5 mb-0 ml-12 px-7 py-7">
+                <div className="text-black font-sans font-medium text-[40px] tracking-[-0.01em] w-[428px]">
+                  COUTURE MAKEUP COLLECTION 2017
+                </div>
+                <div className="font-medium text-[20px] tracking-[0.01em] text-black w-[271px]">
+                  Для людей, которые знают, что такое росскошь
+                </div>
+                <div className="">
+                  <Image src="./first.svg" width={496} height={496}></Image>
+                </div>
+                <div className="flex items-center justify-between w-[500px]">
+                  <div className="font-bold text-4xl tracking-tight text-black">
+                    29 000 ₽
+                  </div>
+                  <button className="bg-[#600027] rounded-[52px] w-[157px] h-[63px]  text-[25px] translate-x-[-20px]">
+                    Купить
+                  </button>
+                </div>
+              </div>
+            </div>
+          </>
+          <div className="text-4xl m-[90px]">Свяжитесь с нами</div>
+          <div className="h-[456px] w-full bg-[#600027] my-[-60px] flex ">
+            <div className="w-full ml-[90px] mr-[90px]">
+              <div className="text-3xl mt-[30px]">
+                Получите полную консультацию
+              </div>
+              <div className="w-full h-[3px] bg-white mt-[30px]"></div>
+              <div className="">
+                <div className="flex justify-between mr-[200px] mt-[30px]">
+                  <div className="">
+                    <div className="text-[40px]">
+                      Сходите в один из ближайших магазинов
+                    </div>
+                    <button className="border-2 border-white rounded-[52px] w-[372px] h-[75px] text-[30px] mt-[30px]">
+                      Найти на карте
+                    </button>
+                  </div>
+                  <div className="">
+                    <div className="text-[40px]">Позвоните по телефону</div>
+                    <div className="text-[40px]">8 800 555-35-35</div>
+                    <div className="flex gap-[20px] mt-[20px]">
+                      <Image src="./vk.svg" width={36} height={36} />
+                      <Image src="./tg.svg" width={36} height={36} />
+                      <Image src="./fb.svg" width={36} height={36} />
+                      <Image src="./insta.svg" width={36} height={36} />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </>
+        <div class="text-white text-[2.25rem] tracking-wide ml-[90px] mt-[140px]">
+          Частые вопросы (FAQ)
+        </div>
+        <div className="flex ">
+          <ol className="ml-[90px] mt-[40px]">
+            <li className="text-[25px]">
+              1. Как долго идет доставка?
+              <div className="text-[20px] ml-[27px] text-gray-300">
+                Все зависит от вашего города. Если заказ был сделан в городе,
+                где есть склад, то втечении 2 дней.
+              </div>
+            </li>
+            <li className="text-[25px] mt-[30px]">
+              2. Сколько хватит по времени набора?
+              <div className="text-[20px] ml-[27px] text-gray-300">
+                Хватает на 5-6 лет активного использования.
+              </div>
+            </li>
+            <li className="text-[25px] mt-[30px]">
+              3. Можно ли взять в рассрочку или кредит?
+              <div className="text-[20px] ml-[27px] text-gray-300">
+                Конечно можно, но лучше не надо;)
+              </div>
+            </li>
+            <li className="text-[25px] mt-[30px]">
+              4. Могу ли я проконсультироваться в магизине?
+              <div className="text-[20px] ml-[27px] text-gray-300">
+                Конечно! Сходите в любой ближайший магазин нашего бренда и
+                спросите любого консультанта.
+              </div>
+            </li>
+          </ol>
+        </div>
+      </>
+      <divc className="h-[3px] w-full">
+        <div className="bg-white h-[3px] ml-[90px] mr-[90px] mt-[30px]"></div>
+      </divc>
+    </>
+  );
+}
